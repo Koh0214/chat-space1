948c676fb89c5aa4f0bac08381d2893c1397b7ae
岸本 晃平2 <Kohei2@koheimba.local>
2017-01-09 13:35:33 +0900

    rollbackし、foreign_keyを削除後に再びmigrate

--- db/migrate/20170109034528_create_group_users.rb
+++ db/migrate/20170109034528_create_group_users.rb
@@ -1,8 +1,8 @@
 class CreateGroupUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :group_users do |t|
-      t.integer   :group_id, null: false, foreign_key: true
-      t.integer   :user_id,  null: false, foreign_key: true
+      t.integer   :group_id, null: false
+      t.integer   :user_id,  null: false
       t.timestamps
     end
   end
