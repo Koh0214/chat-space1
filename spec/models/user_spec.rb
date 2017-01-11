03d366cccd251587389589965ba280c7103cfa89
岸本 晃平2 <Kohei2@koheimba.local>
2017-01-11 18:21:04 +0900

    contextを使い、valid、invalidで分け、invalidの中をさらに細分化した

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,51 +1,59 @@
 require 'rails_helper'
 
 describe User do
-  it "is valid with a nickname, email, password, password_confirmation" do
-    user = build(:user)
-    expect(user).to be_valid
-  end
+  context "valid" do
+    it "is valid with a nickname, email, password, password_confirmation" do
+      user = build(:user)
+      expect(user).to be_valid
+    end
 
-  it "is valid with a password that has more than 8 characters " do
-    user = build(:user, password: "12345678", password_confirmation: "12345678")
-    expect(user).to be_valid
+    it "is valid with a password that has more than 8 characters " do
+      user = build(:user, password: "12345678", password_confirmation: "12345678")
+      expect(user).to be_valid
+    end
   end
+  
+  context "invalid" do
+    context "without something" do
+      it "is invalid without a name" do
+        user = build(:user, name: "")
+        user.valid?
+        expect(user.errors[:name]).to include("が入力されていません。")
+      end
 
-  it "is invalid without a name" do
-  	user = build(:user, name: "")
-  	user.valid?
-  	expect(user.errors[:name]).to include("が入力されていません。")
-  end
+      it "is invalid without a email" do
+        user = build(:user, email: "")
+        user.valid?
+        expect(user.errors[:email]).to include("が入力されていません。")
+      end
 
-  it "is invalid without a email" do
-    user = build(:user, email: "")
-    user.valid?
-    expect(user.errors[:email]).to include("が入力されていません。")
-  end
+      it "is invalid without a password" do
+        user = build(:user, password: "")
+        user.valid?
+        expect(user.errors[:password]).to include("が入力されていません。")
+      end
 
-  it "is invalid without a password" do
-    user = build(:user, password: "")
-    user.valid?
-    expect(user.errors[:password]).to include("が入力されていません。")
-  end
+      it "is invalid without a password_confirmation" do
+        user = build(:user, password_confirmation: "")
+        user.valid?
+        expect(user.errors[:password_confirmation][0]).to include("が内容とあっていません。")
+      end
+    end
 
-  it "is invalid without a password_confirmation" do
-    user = build(:user, password_confirmation: "")
-    user.valid?
-    expect(user.errors[:password_confirmation][0]).to include("が内容とあっていません。")
-  end
+    context "invalid others" do
+      it "is invalid with a duplicate email address" do
+        user = create(:user)
+        another_user = build(:user, email: user.email)
+        another_user.valid?
+        expect(another_user.errors[:email]).to include("は既に使用されています。")
+      end
 
-  it "is invalid with a duplicate email address" do
-    user = create(:user)
-    another_user = build(:user, email: user.email)
-    another_user.valid?
-    expect(another_user.errors[:email]).to include("は既に使用されています。")
-  end
-
-  it "is invalid with a password that has less than 8 characters " do
-    user = build(:user, password: "1234567")
-    user.valid?
-    expect(user.errors[:password][0]).to include("は8文字以上に設定して下さい。")
+      it "is invalid with a password that has less than 8 characters " do
+        user = build(:user, password: "1234567")
+        user.valid?
+        expect(user.errors[:password][0]).to include("は8文字以上に設定して下さい。")
+      end
+    end    
   end
 end
 
