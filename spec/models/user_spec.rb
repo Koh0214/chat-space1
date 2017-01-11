ec4640ac36c82985c7fbc29831a428104321ddc7
岸本 晃平2 <Kohei2@koheimba.local>
2017-01-11 15:35:50 +0900

    nameとemailをまとめ、describe #createのネストを削除

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,22 +1,15 @@
 require 'rails_helper'
 
 describe User do
-  describe '#create' do
-    it "is invalid without a name" do
-    	user = User.new(name: "", email: "kkk@gmail.com" , password: "00000000", password_confirmation: "00000000")
-    	user.valid?
-    	expect(user.errors[:name]).to include("が入力されていません。")
-    end
+  it "is invalid without a name" do
+  	user = User.new(name: "", email: "kkk@gmail.com" , password: "00000000", password_confirmation: "00000000")
+  	user.valid?
+  	expect(user.errors[:name]).to include("が入力されていません。")
+  end
+
+  it "is invalid without a email" do
+    user = User.new(name: "kishi", email: "" , password: "00000000", password_confirmation: "00000000")
+    user.valid?
+    expect(user.errors[:email]).to include("が入力されていません。")
   end
 end
-
-describe User do
-  describe '#create' do
-    it "is invalid without a email" do
-    	user = User.new(name: "kishi", email: "" , password: "00000000", password_confirmation: "00000000")
-    	user.valid?
-    	expect(user.errors[:email]).to include("が入力されていません。")
-    end
-  end
-end
-
