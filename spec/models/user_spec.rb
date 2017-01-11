ebab34959c0492f3d8fc88ee41954201f2c6f810
岸本 晃平2 <Kohei2@koheimba.local>
2017-01-11 15:02:45 +0900

    user_rspec.rbの記述を日本語に変更

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,7 @@ describe User do
     it "is invalid without a name" do
     	user = User.new(name: "", email: "kkk@gmail.com" , password: "00000000", password_confirmation: "00000000")
     	user.valid?
-    	expect(user.errors[:name]).to include("can't be blank")
+    	expect(user.errors[:name]).to include("が入力されていません。")
     end
   end
 end
@@ -15,7 +15,7 @@ describe User do
     it "is invalid without a email" do
     	user = User.new(name: "kishi", email: "" , password: "00000000", password_confirmation: "00000000")
     	user.valid?
-    	expect(user.errors[:email]).to include("can't be blank")
+    	expect(user.errors[:email]).to include("が入力されていません。")
     end
   end
 end
