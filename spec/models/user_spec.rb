ea40c9026134395af77868a39c4a2d5f2d58c279
岸本 晃平2 <Kohei2@koheimba.local>
2017-01-11 16:13:57 +0900

    factory_girlを使って書き換え

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,19 +2,19 @@ require 'rails_helper'
 
 describe User do
   it "is invalid without a name" do
-  	user = User.new(name: "", email: "kkk@gmail.com" , password: "00000000", password_confirmation: "00000000")
+  	user = build(:user, name: "")
   	user.valid?
   	expect(user.errors[:name]).to include("が入力されていません。")
   end
 
   it "is invalid without a email" do
-    user = User.new(name: "kishi", email: "" , password: "00000000", password_confirmation: "00000000")
+    user = build(:user, email: "")
     user.valid?
     expect(user.errors[:email]).to include("が入力されていません。")
   end
 
   it "is invalid without a password" do
-    user = User.new(name: "kishi", email: "kkk@gmail.com" , password: "", password_confirmation: "00000000")
+    user = build(:user, password: "")
     user.valid?
     expect(user.errors[:password]).to include("が入力されていません。")
   end
