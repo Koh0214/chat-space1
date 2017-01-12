bc60805e199b982e03e6cbb64711915ddd33e720
岸本 晃平2 <Kohei2@koheimba.local>
2017-01-12 17:18:41 +0900

    Groupのnameだけ入れられた。GroupUserはエラー。
    配列の周りの処理がいけないのかなと思う。

--- app/controllers/chat_groups_controller.rb
+++ app/controllers/chat_groups_controller.rb
@@ -5,14 +5,17 @@ class ChatGroupsController < ApplicationController
 	def create
 		set_params_group
 		set_params_chat_group
+		binding.pry
 		redirect_to :root
 	end
 
 	private
 	def set_params_group 
 		params.require(:group).permit(:group_users => [])
+		# GroupUser.create(user_id: params[:group][:group_users])
 	end
 	def set_params_chat_group
 		params.require(:chat_group).permit(:name, :user_ids => [])
+		Group.create(name: params[:chat_group][:name])
 	end
 end
