9d5f0d2356f7cfd25d38277a2e41812cd341f310
岸本 晃平2 <Kohei2@koheimba.local>
2017-01-12 19:02:14 +0900

    set_params一つに統合、binding.pryの削除

--- app/controllers/chat_groups_controller.rb
+++ app/controllers/chat_groups_controller.rb
@@ -1,21 +1,19 @@
 class ChatGroupsController < ApplicationController
+	
 	def new
 		@group = Group.new
 	end
 
 	def create
-		set_params_group
-		set_params_chat_group
-		binding.pry
+		set_params
 		redirect_to :root
 	end
 
 	private
-	def set_params_group 
+	def set_params
 		params.require(:group).permit(:group_users => [])
-	end
-	def set_params_chat_group
 		params.require(:chat_group).permit(:name, :user_ids => [])
 		Group.create(name: params[:chat_group][:name])
 	end
+
 end
