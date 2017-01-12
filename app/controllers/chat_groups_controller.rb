00becc5cc74996a3903c483dbbfb64e0ca43b2bb
岸本 晃平2 <Kohei2@koheimba.local>
2017-01-12 13:58:19 +0900

    ストロングパラメーターを設定

--- app/controllers/chat_groups_controller.rb
+++ app/controllers/chat_groups_controller.rb
@@ -3,7 +3,17 @@ class ChatGroupsController < ApplicationController
 	end
 
 	def create
+		set_params
+		set_params2
 		binding.pry
 		redirect_to :root
+	end
+
+	private
+	def set_params 
+		params.require(:group).permit(:group_users => [])
+	end
+	def set_params2
+		params.require(:chat_group).permit(:name, :user_ids => [])
 	end
 end
