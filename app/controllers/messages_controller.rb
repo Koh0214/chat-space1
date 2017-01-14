772584232ad4778783c1e366d182e12106ef214d
岸本 晃平2 <Kohei2@koheimba.local>
2017-01-14 21:21:26 +0900

    strong_parameterの設定（ムリヤリ）

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,5 +3,22 @@ class MessagesController < ApplicationController
   	# binding.pry
   	@groups = Group.all
   	@group = Group.find(params[:group_id])
+  	@message = Message.new
+  end
+
+  def new
+  end
+
+  def create
+  	message = Message.create(set_params)
+		# redirect_to :root
+  end
+
+  private
+  def set_params
+  	params[:user_id] = current_user.id
+  	params1 = params.require(:message).permit(:body)
+  	params2 = params.permit(:group_id, :user_id)
+  	final_params = params1.merge(params2)
   end
 end
