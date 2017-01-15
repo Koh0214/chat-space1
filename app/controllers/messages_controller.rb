eed8c6929608163928fec2e53fce60d5f27cf90c
岸本 晃平2 <Kohei2@koheimba.local>
2017-01-15 14:23:54 +0900

    いらないコメントアウトの削除

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,28 +1,25 @@
 class MessagesController < ApplicationController
   def index
-  	# binding.pry
-  	@groups = Group.all
-  	@group = Group.find(params[:group_id])
-  	@message = Message.new
+    @groups = Group.all
+    @group = Group.find(params[:group_id])
+    @message = Message.new
   end
 
   def create
-  	case params[:message][:body].empty?
-  	when true
-  		# window.alert("何か書かないと送信できません")
-  		redirect_to request.original_url, notice: '何か書かないと送信できません'
-  		# flash.now[:notice] = '何か書かないと送信できません'
-  	when false
-	  	message = Message.create(set_params)
-	  	redirect_to request.original_url
-	  end
+    case params[:message][:body].empty?
+    when true
+      redirect_to request.original_url, notice: '何か書かないと送信できません'
+    when false
+      message = Message.create(set_params)
+      redirect_to request.original_url
+    end
   end
 
   private
   def set_params
-  	params[:user_id] = current_user.id
-  	params1 = params.require(:message).permit(:body)
-  	params2 = params.permit(:group_id, :user_id)
-  	final_params = params1.merge(params2)
+    params[:user_id] = current_user.id
+    params1 = params.require(:message).permit(:body)
+    params2 = params.permit(:group_id, :user_id)
+    final_params = params1.merge(params2)
   end
 end
