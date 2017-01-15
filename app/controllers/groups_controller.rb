6d5b654e6a879858e95f99819ed49ce4dc4b33a3
岸本 晃平2 <Kohei2@koheimba.local>
2017-01-15 14:18:48 +0900

    インデントをtab => spaceに修正

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,32 +1,32 @@
 class GroupsController < ApplicationController
 
-	def index
-		@groups = Group.all
-	end
+  def index
+    @groups = Group.all
+  end
 
-	def new
-		@group = Group.new
-	end
+  def new
+    @group = Group.new
+  end
 
-	def edit
-		@group = Group.find(params[:id])
-	end
+  def edit
+    @group = Group.find(params[:id])
+  end
 
-	def create
-		Group.create(set_params)
-		@user_ids = set_params[:user_ids]
-		redirect_to :root
-	end
+  def create
+    Group.create(set_params)
+    @user_ids = set_params[:user_ids]
+    redirect_to :root
+  end
 
-	def update
-		group = Group.find(params[:id])
-		group.update(set_params)
-		redirect_to :root
-	end
+  def update
+    group = Group.find(params[:id])
+    group.update(set_params)
+    redirect_to :root
+  end
 
-	private
-	def set_params
-		params.require(:group).permit(:name, { user_ids: [] })
-	end
+  private
+  def set_params
+    params.require(:group).permit(:name, { user_ids: [] })
+  end
 
 end
