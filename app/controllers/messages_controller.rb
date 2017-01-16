44c7ab80320dc4d99616d7623f0c4e568dede5a5
岸本 晃平2 <Kohei2@koheimba.local>
2017-01-16 13:15:22 +0900

    アソシエーションを使ってリファクタリング
    @groups = current_user.groups
    で、current_userの持つgroupsを取ってくる
    
    message = current_user.messages.create(set_params)
    
    でcurrent_userのidを入れたmessageをcreateする。
    
    set_paramsの中身は
    [:message[:body]]と、[group_id: params[:group_id]]の2つ。
    これにcurrent_userから来たidが加わる。

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,24 +1,22 @@
 class MessagesController < ApplicationController
   def index
-    @groups = Group.all
     @group = Group.find(params[:group_id])
+    @groups = current_user.groups
     @message = Message.new
   end
 
   def create
-    if params[:message][:body].empty?
-      redirect_to group_messages_path, notice: '何か書かないと送信できません'
-    else
-      message = Message.create(set_params)
+    if params[:message][:body].present?
+      message = current_user.messages.create(set_params)
       redirect_to group_messages_path
+    else
+      redirect_to group_messages_path, notice: '何か書かないと送信できません'
     end
   end
 
   private
+
   def set_params
-    params[:user_id] = current_user.id
-    params1 = params.require(:message).permit(:body)
-    params2 = params.permit(:group_id, :user_id)
-    final_params = params1.merge(params2)
+    params.require(:message).permit(:body).merge(group_id: params[:group_id])
   end
 end
