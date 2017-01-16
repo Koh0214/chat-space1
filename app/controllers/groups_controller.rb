844a6bacd9b8396ce456f4ef025916c5e13c2788
岸本 晃平2 <Kohei2@koheimba.local>
2017-01-16 17:51:41 +0900

    サインインしているユーザーのグループのみ表示、セーブできるか分岐、new&create, edit&updateでまとめた

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,21 +1,29 @@
 class GroupsController < ApplicationController
 
   def index
-    @groups = current_user.groups
+    if user_signed_in?
+      @groups = current_user.groups
+    else
+      redirect_to new_user_session_path
+    end
   end
 
   def new
     @group = Group.new
   end
 
-  def edit
-    @group = Group.find(params[:id])
+  def create 
+    group = Group.new(set_params)
+    if group.save
+      @user_ids = set_params[:user_ids]
+      redirect_to :root
+    else
+      redirect_to new_group_path, alert: 'グループ名と1名以上のメンバーは必須です'
+    end
   end
 
-  def create
-    Group.create(set_params)
-    @user_ids = set_params[:user_ids]
-    redirect_to :root
+  def edit
+    @group = Group.find(params[:id])
   end
 
   def update
