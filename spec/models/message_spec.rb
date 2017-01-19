a814d261c88aab3be3eb40fba11ef2241faa386b
岸本 晃平2 <Kohei2@koheimba.local>
2017-01-19 20:58:40 +0900

    model testの英文の修正

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,27 +1,27 @@
 require 'rails_helper'
 
 describe Message do
-  context "valid" do
-    it "is valid with a body, user_id, group_id" do
+  context "is valid" do
+    it "with a body, user_id, group_id" do
       message = build(:message)
       expect(message).to be_valid
     end
   end
 
-  context "invalid" do
-    it "is invalid without body" do
+  context "is invalid" do
+    it "without body" do
       message = build(:message, body:"")
       message.valid?
       expect(message.errors[:body]).to include("が入力されていません。")
     end
 
-    it "is invalid without group_id" do
+    it "without group_id" do
       message = build(:message, group_id: nil)
       message.valid?
       expect(message.errors[:group_id]).to include("が入力されていません。")
     end
 
-    it "is invalid without user_id" do
+    it "without user_id" do
       message = build(:message, user_id: nil)
       message.valid?
       expect(message.errors[:user_id]).to include("が入力されていません。")
