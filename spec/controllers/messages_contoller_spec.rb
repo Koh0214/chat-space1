6f8fec6d4d8a90ee43b620c57c2047809e0d6e1b
岸本 晃平2 <Kohei2@koheimba.local>
2017-01-20 17:18:37 +0900

    DeviseのTestHelpersを読み込み
    これによって、sign_inとかのメソッドが使える様になるっぽい。

--- spec/controllers/messages_contoller_spec.rb
+++ spec/controllers/messages_contoller_spec.rb
@@ -2,19 +2,13 @@ require 'rails_helper'
 
 describe MessagesController do
 
-  # let(:user) { FactoryGirl.build(:user) }
-  #
-  # before do
-  #   login_user user
-  # end
-  #
-  # describe 'GET #index' do
-  #   it "renders the :index template" do
-  #     group = create(:group)
-  #     get :index, params: { group_id: group.id }
-  #     expect(response).to render_template :index
-  #   end
-  # end
+  describe 'GET #index' do
+    it "renders the :index template" do
+      group = create(:group)
+      get :index, params: { group_id: group.id }
+      expect(response).to render_template :index
+    end
+  end
 
   # describe 'POST #create' do
   #   it "renders the :group_messages_path template" do
