4cd40cb9a4990b4a2c5a8e784bbd9279dbbdd5f7
岸本 晃平2 <Kohei2@koheimba.local>
2017-01-21 22:30:52 +0900

    groupとmessageのFaker::Timeに整合性をもたせた

--- spec/factories/message.rb
+++ spec/factories/message.rb
@@ -4,6 +4,6 @@ FactoryGirl.define do
     group
     body    Faker::Lorem.sentence
     image   "hoge.png"
-    created_at { Faker::Time.between(2.days.ago, Date.today, :all) }
+    created_at { Faker::Time.between(1.days.ago, Date.today, :all) }
   end
 end
