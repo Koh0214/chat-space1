d79388b13713934b1bd643bffaf5c8818d235a29
岸本 晃平2 <Kohei2@koheimba.local>
2017-01-21 11:54:13 +0900

    groupのテストモデルの削除

--- spec/models/group_spec.rb
+++
@@ -1,7 +0,0 @@
-FactoryGirl.define do
-  factory :group do
-    name  "グルーーーープ！"
-    created_at { Faker::Time.between(2.days.ago, Date.today, :all) }
-    updated_at { Faker::Time.between(2.days.ago, Date.today, :all) }
-  end
-end
