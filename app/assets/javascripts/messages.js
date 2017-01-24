2f0fdc9f4dc20093bd4d42d8789b8924d1f2a602
岸本 晃平2 <Kohei2@koheimba.local>
2017-01-24 16:01:49 +0900

    post_name, timestamp, postを非同期通信で実現

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,9 +1,23 @@
 $(function() {
+  function build_post_name(name) {
+    var html = $('<h5 class="post_name">').append(name);
+    return html;
+  }
+  function build_timestamp(data) {
+    var html = $('<p class="timestamp">').append(data.created_at);
+    return html;
+  }
+  function build_post(data) {
+    var html = $('<p class="post">').append(data.body);
+    return html;
+  }
+
   $('.new_message').on('submit', function(e) {
     e.preventDefault();
     var textField = $('.form');
     var input_message = textField.val();
     var group_id = $('.group_id').val();
+    var name = $('.current_user_name').val();
     $.ajax({
       type: 'POST',
       url: '/groups/' + group_id + '/messages.json',
@@ -12,7 +26,13 @@ $(function() {
     })
     .done(function(data) {
       console.log('OK!')
-      $('p.post').append(message.body);
+      var post_name = build_post_name(name);
+      $('div.right__content').append(post_name);
+      var timestamp = build_timestamp(data);
+      $('div.right__content').append(timestamp);
+      var post = build_post(data);
+      $('div.right__content').append(post);
+      textField.val('');
     })
     .fail(function(data) {
       console.log('保存できとらんゾーーーーー');
