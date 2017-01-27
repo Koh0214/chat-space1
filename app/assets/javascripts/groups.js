0c2f203d6b5ec6db3e8dcb956e461e27836258e1
岸本 晃平2 <Kohei2@koheimba.local>
2017-01-27 22:04:32 +0900

    add ajax list and activate click action

--- app/assets/javascripts/groups.js
+++ app/assets/javascripts/groups.js
@@ -1,4 +1,17 @@
 $(function() {
+  var list = $("#list");
+
+  function appendList(user) {
+    var searched_user = $('<div class="searched_user">').append(user);
+    var add_button = $('<a href="javascript:void(0)" class="add_button" >').append("追加");
+    var box = $('<li class="box">').append(searched_user, add_button);
+    list.append(box);
+  };
+
+  $('body').on('click', '#list .add_button', function() {
+    console.log("uhyaaaaaa");
+  });
+
   $('#user-search-field').on('keyup', function(e) {
     e.preventDefault();
     var textField = $('#user-search-field');
@@ -10,12 +23,17 @@ $(function() {
       dataType: 'json'
     })
     .done(function(users) {
-      // function(data)の引数に、コントローラーから値が返ってくる
+      // function(users)の引数に、コントローラーから値が返ってくる
       console.log('いいゾーーーー！！');
-      console.log(users);
       $.each(users,
         function(index, user) {
           console.log(user.name);
+        }
+      )
+      $(".box").remove();
+      $.each(users,
+        function(index, user) {
+          appendList(user.name);
         }
       )
     })
