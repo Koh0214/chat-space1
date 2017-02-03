dcc3e913376632af67ad7c2002bc10257c78fa92
岸本 晃平2 <Kohei2@koheimba.local>
2017-02-03 22:08:06 +0900

    message.rb | set validattion present body or image

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,5 +4,13 @@ class Message < ApplicationRecord
 
   mount_uploader :image, ImageUploader
 
-  # validates :body, presence: true
+
+  validate :present_body_or_image
+
+  def present_body_or_image
+    if !body.present? && !image.present?
+      errors.add(:body, "can't be in the past")
+    end
+  end
+
 end
