03c89b83640f8c3a0a324dacb78aa19972415a77
岸本 晃平2 <Kohei2@koheimba.local>
2017-02-08 19:35:29 +0900

    messages_controller.rb | fix for no message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,7 +9,7 @@ class MessagesController < ApplicationController
     @messages = @group.messages
 
     last_message = @group.messages.last
-    gon.last_message_name = last_message.user.name
+    gon.last_message_name = last_message.user.name if last_message.present?
     gon.group_id = @group.id
 
     respond_to do |format|
