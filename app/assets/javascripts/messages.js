71eb21f09f7f8440b06e2a1f9f686208f893822a
岸本 晃平2 <Kohei2@koheimba.local>
2017-02-08 11:24:10 +0900

    messages.js | remove js to set right__content height

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -38,9 +38,4 @@ $(function() {
     });
   });
 
-  $(window).load(function(){
-    var left_height = $('.left__content').height();
-    $('.right__content').height(left_height - 120);
-  })
-
 });
