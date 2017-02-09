a6bfcf4f9a365b821a12a60c037f30fc9a8b7642
岸本 晃平2 <Kohei2@koheimba.local>
2017-02-09 23:02:42 +0900

    production.rb | set server information

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,5 @@
+server '13.112.37.192', user: 'ec2-user', roles: %w{app db web}
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
