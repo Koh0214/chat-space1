713f5d6d35183dc82d6356ed2d445ea7b1b96185
岸本 晃平2 <Kohei2@koheimba.local>
2017-02-10 12:06:50 +0900

    deploy.rb | fix GitHub address

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "3.7.2"
 
 set :application, 'rails-capistrano-test'
-set :repo_url, 'git@github.com:chat-space1.git'
+set :repo_url, 'git@github.com:Koh0214/chat-space1.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
