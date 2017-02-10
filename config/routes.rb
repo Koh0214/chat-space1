d49b89f4d10fc2eed34902b5285299159cfdc010
岸本 晃平2 <Kohei2@koheimba.local>
2017-02-10 17:25:49 +0900

    routes.rb | json => html

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users, defaults: { format: "json" }
-  root 'groups#index', defaults: { format: "json" }
+  devise_for :users, defaults: { format: "html" }
+  root 'groups#index', defaults: { format: "html" }
   resources :groups, only: [:new, :create, :edit, :update] do
-    resources :messages, only: [:index, :create], defaults: { format: "json" }
+    resources :messages, only: [:index, :create], defaults: { format: "html" }
   end
   resources :users, only: :index
 end
